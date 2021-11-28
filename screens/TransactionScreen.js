4fa304c1d1117e042b4324c8c72b8b9cba6c61ce
Shifra3 <72134790+Shifra3@users.noreply.github.com>
2021-11-28 11:53:53 +0530

    Add files via upload

---
+++ screens/TransactionScreen.js
@@ -0,0 +1,130 @@
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import * as Permissions from "expo-permissions"
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity,TextInput } from 'react-native';
+import * as firebase from "firebase"
+import db from "../config"
+
+export default class TransactionScreen extends React.Component {
+  constructor(){
+    super(
+      this.state={
+      hascamerapermissions:null,
+      scanned:false,
+      scanneddata:"",
+      buttonState:"normal" ,
+      bookid:"",
+      studentid:"" ,
+      
+      
+      }
+    )
+  }
+  handleTransaction=async()=>{
+    var TransactionMessage
+    db.collection("books").doc(this.state.bookid).get()
+    .then(doc=>{
+      var book=doc.data()
+      if(book.bookAvailability){
+        this.initiateBookIssue()
+        TransactionMessage="bookIssued"
+      }
+      else{this.initiatebookReturn()
+      TransactionMessage="bookReturn"
+      }
+
+      
+    })
+  }
+getCameraPermissions=async(buttonState)=>{
+  const {status}=await Permissions.askAsync(PERMISSIONS.CAMERA)
+this.setState({
+  hascamerapermissions:status==="granted",
+  buttonState:"buttonState",
+  scanned:false
+})
+}
+
+handleBarCodeScanned=async({type,data})=>{
+  if(this.state.buttonState==="bookid"){
+
+  
+  this.setState({
+    scanned:true,
+    scanneddata:data,
+    buttonState:"normal",
+    bookid:data
+  })
+}
+else if(this.state.buttonState){
+  this.setState({
+  scanned:true,
+    scanneddata:data,
+    buttonState:"normal",
+    studentid:data
+  })
+
+}
+}  
+
+    render(){
+if(this.state.buttonState!=="normal" && this.state.hascamerapermissions){
+return(
+  <BarCodeScanner
+  onBarCodeScanned={scanned?undefined:this.handleBarCodeScanned}
+  style={
+    StyleSheet.absoluteFillObject
+  }
+  ></BarCodeScanner>
+)
+}
+
+else if(this.state.buttonState==="normal"){
+
+
+  return (
+    <View style={styles.container}>
+      <Text>TransactionScreen</Text>
+      <Text>
+        {this.state.hascamerapermissions===true? this.state.scanneddata:"request camera permission"}
+      </Text>
+      <View>
+        <TextInput
+        placeholder={"bookid"}
+       value={this.state.bookid}
+        /> 
+<TouchableOpacity
+onPress={()=>this.getCameraPermissions("bookid")}
+>
+  <Text>Scan </Text>
+</TouchableOpacity>
+</View>
+<View>
+        <TextInput
+        placeholder={"studentid"}
+       value={this.statestudentid}
+        /> 
+<TouchableOpacity
+onPress={()=>this.getCameraPermissions("studentid")}
+>
+  <Text>Scan </Text>
+</TouchableOpacity>
+</View>
+<TouchableOpacity
+onPress={()=>this.handleTransaction()}
+>
+  <Text>Submit </Text>
+</TouchableOpacity>
+    </View>
+  );
+}
+}
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
